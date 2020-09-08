ffb7e4c4b807fda751669ac214cdf652af9296db
Thierry Delahaye <delahaye.thierry@gmail.com>
2020-09-08 23:46:12 +0700

    Create indexes !

---
+++ create_indexes.py
@@ -0,0 +1,51 @@
+import math
+import pandas as pd
+import utils
+
+import utils
+
+
+# Main method
+def create_indexes():
+
+    # Define the indexes by weights
+    indexes = {
+        'lighting': {
+            'accidents': 0.5,
+            'hotels': 0.5
+        },
+        'connectivity': {
+            'hotels': 1
+        },
+        'security': {
+            'accidents': 1
+        },
+    }
+
+    # Loop over indexes and build the index from the existing data
+    for index in indexes:
+        index_values = pd.DataFrame(columns=utils.get_index_headers())
+        for data_source in indexes[index]:
+            file_name = f'output_data/{data_source}.csv'
+            data_source_content = pd.read_csv(file_name)
+            index_values = index_values.merge(data_source_content, how='outer', on=utils.get_index_headers())
+
+        # Merge the datasources into the index
+        index_values = index_values.fillna(value=0)
+        index_values['Index'] = index_values.dot(pd.Series(indexes[index]).reindex(index_values.columns, fill_value=0))
+
+        # Write the index to the map file
+        print(f'Writing index file for {index}')
+        index_values.to_csv(f'map/{index}_needs_index.csv', index=False)
+
+
+    return
+
+
+
+
+# Module execution: launch main method
+if __name__ == '__main__':
+    
+    create_indexes()
+
