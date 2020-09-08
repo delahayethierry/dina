3dbc9938146495cc3c1e1de4abf6c541ba69238d
Thierry Delahaye <delahaye.thierry@gmail.com>
2020-09-09 00:40:28 +0700

    Year and month implemented

--- get_accidents_geodata.py
+++ get_accidents_geodata.py
@@ -1,3 +1,4 @@
+import datetime
 import math
 import utils
 
@@ -13,10 +14,6 @@ def get_accidents_geodata(input_file_accidents):
     lines_read = 0
     lines_written = 0
 
-    # Dummy year and month variables
-    year = '2020'
-    month = '01'
-    
     # Open the accidents file
     with open(input_file_accidents) as filin_accidents:
         
@@ -34,16 +31,27 @@ def get_accidents_geodata(input_file_accidents):
                 accident_block_details = utils.extract_block(line_dict['Longitudine'], line_dict['Latitudine'])
                 accident_block_name = accident_block_details['name']
                 accident_lighting = line_dict['Illuminazione']
+                accident_datetime_str = line_dict['DataOraIncidente']
+                try:
+                    accident_datetime = datetime.datetime.strptime(accident_datetime_str, '%d/%m/%Y %H:%M:%S')
+                    accident_year = str(accident_datetime.year)
+                    accident_month = str(accident_datetime.month)
+                except:
+                    accident_year = '-1'
+                    accident_month = '-1'
+                accident_year_month = (accident_year, accident_month)
                 
                 # Add the accident to the statistics
                 if accident_block_name not in accidents_per_block:
-                    accidents_per_block[accident_block_name] = accident_block_details
-                if accident_lighting not in accidents_per_block[accident_block_name]:
-                    accidents_per_block[accident_block_name][accident_lighting] = 0
-                accidents_per_block[accident_block_name][accident_lighting] += 1
+                    accidents_per_block[accident_block_name] = {}
+                if accident_year_month not in accidents_per_block[accident_block_name]:
+                    accidents_per_block[accident_block_name][accident_year_month] = accident_block_details
+                if accident_lighting not in accidents_per_block[accident_block_name][accident_year_month]:
+                    accidents_per_block[accident_block_name][accident_year_month][accident_lighting] = 0
+                accidents_per_block[accident_block_name][accident_year_month][accident_lighting] += 1
                 if accident_lighting == 'Insufficiente':
-                    if accidents_per_block[accident_block_name][accident_lighting] > max_accidents_per_block:
-                        max_accidents_per_block = int(accidents_per_block[accident_block_name][accident_lighting])
+                    if accidents_per_block[accident_block_name][accident_year_month][accident_lighting] > max_accidents_per_block:
+                        max_accidents_per_block = int(accidents_per_block[accident_block_name][accident_year_month][accident_lighting])
         
             lines_read += 1
 
@@ -55,12 +63,13 @@ def get_accidents_geodata(input_file_accidents):
 
         # Loop over the blocks and fill the lines
         for accident_block in accidents_per_block:
-            accident_block_details = accidents_per_block[accident_block]
-            accidents_per_block_raw = accident_block_details['Insufficiente'] if 'Insufficiente' in accident_block_details else 0
-            accidents_per_block_index = math.floor((float(accidents_per_block_raw) / float(max_accidents_per_block)) * 10)
-            line_out_elements = [str(i) for i in [accident_block_details['latitude'], accident_block_details['longitude'], year, month, accidents_per_block_index]]
-            filout.write(','.join(line_out_elements) + '\n')
-            lines_written += 1
+            for accident_year_month in accidents_per_block[accident_block]:
+                accident_block_details = accidents_per_block[accident_block][accident_year_month]
+                accidents_per_block_raw = accident_block_details['Insufficiente'] if 'Insufficiente' in accident_block_details else 0
+                accidents_per_block_index = math.floor((float(accidents_per_block_raw) / float(max_accidents_per_block)) * 10)
+                line_out_elements = [str(i) for i in [accident_block_details['latitude'], accident_block_details['longitude'], accident_year_month[0], accident_year_month[1], accidents_per_block_index]]
+                filout.write(','.join(line_out_elements) + '\n')
+                lines_written += 1
     
     # Print some stats
     print(f'Read lines: {lines_read}')
