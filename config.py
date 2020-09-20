68c5b9821baca89efd5cf4a20bdb8639250d1cc9
Nicolas Hauviller <nhauviller@amadeus.com>
2020-09-21 00:07:20 +0200

    Included features to take into account municipality shapres, + customozed block generation

---
+++ config.py
@@ -0,0 +1,13 @@
+# File where the security indexes will be stored to generate the map 
+security_needs_index_file = './map/security_needs_index.csv'
+# File where the connectivity indexes will be stored to generate the map
+connectivity_needs_index_file = './map/connectivity_needs_index.csv'
+# File where the lighting indexes will be stored to generate the map
+lighting_needs_index_file = './map/lighting_needs_index.csv'
+#GeoJson file where the city "town halls"/Municipi shapes are stored
+municipi_geo = "./input_data/municipi.geojson"  
+#Name of the city for the proof of concept
+city_name = "Roma"
+#Size of the blocks (grid to map a city): width & length in meters
+block_width = 1000
+block_height = 1000
