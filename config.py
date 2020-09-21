7f10763b3e8a374e86562fb5bca0875c3a527a1a
Nicolas Hauviller <nhauviller@amadeus.com>
2020-09-21 10:44:14 +0200

    Refactoring and now municipi is an ID (number) to have a consistant way to identify them

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ connectivity_needs_index_file = './map/connectivity_needs_index.csv'
 # File where the lighting indexes will be stored to generate the map
 lighting_needs_index_file = './map/lighting_needs_index.csv'
 #GeoJson file where the city "town halls"/Municipi shapes are stored
-municipi_geo = "./input_data/municipi.geojson"  
+administrative_subdivision_geojson_file = "./input_data/municipi.geojson"
+#municipi_lookup is a csv file which translates the way a municipi is called in various data sources into a unique ID (a number)
+municipi_lookup = "./input_data/municipi_lookup.csv"
 #Name of the city for the proof of concept
 city_name = "Roma"
 #Size of the blocks (grid to map a city): width & length in meters
--- utils.py
+++ utils.py
@@ -1,4 +1,76 @@
 import math
+import folium
+import config
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from pyproj import Transformer
+import shapely.geometry
+from shapely.geometry import Polygon
+
+import geopandas as gpd
+
+administrative_subdivision_lookup_df = pd.read_csv(config.municipi_lookup,names = ['data', 'id', 'display_name'])
+
+
+# Generate city_grid.geojson file based on a bounding box (SW-NE Coordinates to cover) and width/height
+# as defined in config.py in variables block_width and block_height
+def generate_city_grid():
+
+    # Get the lower-left and upper-right bounds from the Municipi shapes to center the map
+    administrative_subdivisions_shapes_bounds = folium.GeoJson(config.administrative_subdivision_geojson_file).get_bounds()
+    print('Generating a city grid with blocks of ',config.block_width,'meters x ',config.block_height,' meters, using this bounding box: ',administrative_subdivisions_shapes_bounds)
+    # Set up projections
+    # 'epsg:3857' = metric; same as EPSG:900913
+    transformer = Transformer.from_crs('epsg:4326', 'epsg:3857')
+    transformer_back = Transformer.from_crs(
+        'epsg:3857', 'epsg:4326')    # go back to 'epsg:4326'
+    # Create corners of rectangle to be transformed to a grid
+    sw = shapely.geometry.Point(administrative_subdivisions_shapes_bounds[0])
+    ne = shapely.geometry.Point(administrative_subdivisions_shapes_bounds[1])
+
+    # Project corners to target projection
+    transformed_sw = transformer.transform(
+        sw.x, sw.y)  # Transform NW point to 3857
+    transformed_ne = transformer.transform(ne.x, ne.y)  # .. same for SE
+
+    municipi_df = gpd.read_file(config.administrative_subdivision_geojson_file)
+    
+
+    i = 0
+    # Iterate over 2D area
+    gridpoints = []
+    properties_block_ID = []
+    properties_administrative_subdivision = []
+    x = transformed_sw[0]
+    while x < transformed_ne[0]:
+        y = transformed_sw[1]
+        while y < transformed_ne[1]:
+            grid_sw = transformer_back.transform(x, y)
+            grid_se = transformer_back.transform(x+config.block_width, y)
+            grid_ne = transformer_back.transform(
+                x+config.block_width, y+config.block_height)
+            grid_nw = transformer_back.transform(x, y+config.block_height)
+            block_polygon = shapely.geometry.Polygon([
+                [grid_sw[1], grid_sw[0]],
+                [grid_se[1], grid_se[0]],
+                [grid_ne[1], grid_ne[0]],
+                [grid_nw[1], grid_nw[0]]])
+            gridpoints.append(block_polygon)
+            administrative_subdivision = ''
+            for index, row in municipi_df.iterrows():
+                if block_polygon.centroid.within(row['geometry']):
+                    administrative_subdivision = " ".join(row['nome'].split()).strip()
+                    break
+            properties_administrative_subdivision.append(str(get_administrative_division_id_from_name(administrative_subdivision)))
+            properties_block_ID.append(i)
+            i += 1
+            y += config.block_height
+        x += config.block_width
+
+    grid = gpd.GeoDataFrame({'geometry': gridpoints, 'block_ID': properties_block_ID,
+                             'administrative_subdivision': properties_administrative_subdivision}, crs='EPSG:4326')
+    grid.to_file("./input_data/city_grid.geojson", driver='GeoJSON')
+    print ('City Grid save in ./input_data/city_grid.geojson, ',str(i-1),' blocks created')
+    return grid
 
 # Extracts a line as a dictionary. Assumes the number of elements in the line is the same as the number of headers
 def extract_line(headers, line_elements):
@@ -12,6 +84,12 @@ def extract_line(headers, line_elements):
     
     return line_dict
 
+def get_administrative_division_id_from_name(name_in_text):
+    
+    if name_in_text in administrative_subdivision_lookup_df['data'].values:
+        return administrative_subdivision_lookup_df[administrative_subdivision_lookup_df.data == name_in_text].id.index[0]
+    else:
+        return ''
 
 # Maps a longitude, latitude to a block. This method can be replaced to whatever block is needed (more/less granular, or any function of longitude, latitude)
 # This larger method takes strings
@@ -72,3 +150,6 @@ def write_index_headers(datasource_name):
     return ','.join(get_index_headers() + [datasource_name])
 
 
+# Module execution: launch main method
+if __name__ == '__main__':
+    generate_city_grid()
