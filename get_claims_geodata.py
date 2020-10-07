b6a15837f3c96e1ae47a93e519588c8a60559cc0
Thierry Delahaye <delahaye.thierry@gmail.com>
2020-10-07 14:09:53 +0700

    WIP claims parsing

--- get_claims_geodata.py
+++ get_claims_geodata.py
@@ -1,14 +1,16 @@
-from datetime import datetime
 import json
 import math
 import os
 import requests
+from datetime import datetime
+
+# Local imports
 import config
 import utils
 
 
 # Main method
-def get_claims_geodata(input_file_wifi):
+def get_claims_geodata(input_file_claims):
 
     # Initialize statistical dictionaries
     security_related_claims_per_block = {}
@@ -27,11 +29,8 @@ def get_claims_geodata(input_file_wifi):
     # Toggles whether the input is geolocalized
     geolocalized_input = False
 
-    # Cache the street name / number lat/long so we don't query the same address twice
-    addresses_coordinates = {}
-    
     # Open the wifi file
-    with open(input_file_wifi) as filin_claims:
+    with open(input_file_claims) as filin_claims:
         
         while True:
             try:
@@ -52,52 +51,39 @@ def get_claims_geodata(input_file_wifi):
                 # General case: extract the line and parse the data
                 else:
                     line_dict = utils.extract_line(headers, line_elements)
-                    
-                    # Build the address
-                    address_street_type = line_dict['DUG']
-                    address_street_name = line_dict['DUF']
-                    address_street_number = line_dict['CIVICO']
-                    address_full = f'{address_street_number}, {address_street_type} {address_street_name}'
 
                     # Parse the date
-                    session_date = datetime.strptime(line_dict['STARTDATE'].replace(',',''),"%d/%m/%Y")
-                    session_year = session_date.year
-                    session_month = session_date.month
-                    session_year_month = (session_year, session_month)
+                    claims_date = datetime.strptime(line_dict['Data di presentazione'].replace(',',''),"%d/%m/%Y")
+                    claim_year = claims_date.year
+                    claim_month = claims_date.month
+                    claim_year_month = (session_year, session_month)
 
-                    # If no address, do not geolocalize. If there is, try the cache
-                    if address_street_name == '':
-                        geolocalization_success = False
-                    elif geolocalized_input:
-                        latitude = line_dict['latitude']
-                        longitude = line_dict['longitude']
-                        geolocalization_success = True
-                    elif address_full in addresses_coordinates:
-                        latitude, longitude = addresses_coordinates[address_full] 
-                        geolocalization_success = True
-                    else:
-                        geolocalization_success, latitude, longitude = utils.query_geolocalization(address_full, CITY_NAME, COUNTRY_NAME, HERE_API_KEY)
-                        print(f'Geolocalization for {address_full}: {geolocalization_success}, {latitude}, {longitude}')
-                        addresses_coordinates[address_full] = (latitude, longitude)
+                    # Parse other features
+                    claim_administrative_division = line_dict['Municipio di riferimento (tramite geo-localizzazione)']
+                    claim_typecode = line_dict['Argomento - codice']
 
-                    if geolocalization_success:
-                        wifi_block_details = utils.get_city_block(longitude, latitude)
-                        wifi_block_name = wifi_block_details['name']
-                    
-                        # Add the wifi usage to the statistics
-                        if wifi_block_name not in security_related_claims_per_block:
-                            security_related_claims_per_block[wifi_block_name] = wifi_block_details
-                            security_related_claims_per_block[wifi_block_name]['usage_per_month'] = {}
-                        if session_year_month not in security_related_claims_per_block[wifi_block_name]['usage_per_month']:
-                            security_related_claims_per_block[wifi_block_name]['usage_per_month'][session_year_month] = {}
-                            security_related_claims_per_block[wifi_block_name]['usage_per_month'][session_year_month]['wifi_usage'] = 0
-                        security_related_claims_per_block[wifi_block_name]['usage_per_month'][session_year_month]['wifi_usage'] += float(line_dict['DOWNLOAD'])
-                        if security_related_claims_per_block[wifi_block_name]['usage_per_month'][session_year_month]['wifi_usage'] > max_wifi_usage_per_block:
-                            max_wifi_usage_per_block = security_related_claims_per_block[wifi_block_name]['usage_per_month'][session_year_month]['wifi_usage']
+                    # Claim types. 150 is lighting, second list is security linked
+                    claim_type = None
+                    if claim_typecode in ['150']:
+                        claim_type = 'Lighting'
+                    if claim_typecode in ['296','298','258','259','380','48','288','47','319','49','300','50']:
+                        claim_type = 'Security'
 
-                        # Save geolocalization if needed to avoid duplicate API calls
-                        if not geolocalized_input:
-                            filoutclaims_geolocated.write(';'.join(line_elements + [str(latitude), str(longitude)]) + '\n')
+                    # Lighting claim
+                    if claim_typecode in ['150']:
+                        if claim_administrative_division in lighting_related_claims_per_block:
+                            lighting_related_claims_per_block[claim_administrative_division] = 0
+                        lighting_related_claims_per_block[claim_administrative_division] += 1
+                        if lighting_related_claims_per_block[claim_administrative_division] > max_lighting_related_claims_per_block:
+                            max_lighting_related_claims_per_block = lighting_related_claims_per_block[claim_administrative_division]
+
+                    # Security claim
+                    elif claim_typecode in ['296','298','258','259','380','48','288','47','319','49','300','50']:
+                        if claim_administrative_division in security_related_claims_per_block:
+                            security_related_claims_per_block[claim_administrative_division] = 0
+                        security_related_claims_per_block[claim_administrative_division] += 1
+                        if security_related_claims_per_block[claim_administrative_division] > max_security_related_claims_per_block:
+                            max_security_related_claims_per_block = security_related_claims_per_block[claim_administrative_division]
             
                 lines_read += 1
                 if lines_read % 1000 == 0:
